0d861a00d79409206f71b47db7e2f9b938328e08
JP Sheehan <jp.sheehan2@gmail.com>
2016-08-08 17:02:38 -0400

    Improved plotting tool

--- plot.sh
+++ plot.sh
@@ -2,12 +2,18 @@
 
 # defaults
 program="gnuplot"
+number=$(ls -t | grep restart | head -1 | grep -o '[0-9]\+') # most recent number
 
-while getopts ":p:" opt; do
+# inputs
+while getopts ":p:n:" opt; do
    case $opt in
       p)
          # Set which program should be used to plot the data
          program=$OPTARG
+         ;;
+      n)
+         # data set number
+         number=$OPTARG
          ;;
       \?)
          echo "Invalid option: -$OPTARG" >&2
@@ -22,11 +28,15 @@ done
 shift $((OPTIND-1))
 parameters=("$@")
 
+if [ $program == "gnuplot" ]
+then
+   script="./plot/plasmaplot.gp"
+fi
+
 for p in "${parameters[@]}"; do
-   script="./plot/${program}_${p}.gp"
-   echo $program
-   echo $script
-   cmd="$program $script"
+   #script="./plot/${program}_${p}.gp"
+   vars="fnum=$number; param='$p'"
+   cmd="$program -e \"$vars\" $script"
    eval $cmd #run script in this terminal
    # gnome-terminal -e sh $script #run script from new terminal (to have multiple windows)
 done
